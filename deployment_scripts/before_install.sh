0c5d68a30c3597193bee62dd9543cfeb5c118cc1
biswanathdas1995 <39395631+biswanathdas1995@users.noreply.github.com>
2022-03-11 15:05:51 +0530

    Update before_install.sh

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-   cd /home/ubuntu/aws-nodejs-sample-codebuild
-   pm2 stop --silent app.js
-  cd /home/ubuntu/
-  sudo mv -rf aws-nodejs-sample-codebuild aws-nodejs-sample-codebuild.backup
-  sudo mkdir aws-nodejs-sample-codebuild
+cd /home/ubuntu/aws-nodejs-sample-codebuild
+pm2 stop --silent app.js
+cd /home/ubuntu/
+sudo mv -rf aws-nodejs-sample-codebuild aws-nodejs-sample-codebuild.backup
+sudo mkdir aws-nodejs-sample-codebuild
