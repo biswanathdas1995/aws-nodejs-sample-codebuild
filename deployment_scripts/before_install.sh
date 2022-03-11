480dd4157fc6565d45469cd03e952c0a5cec15a9
biswanathdas1995 <39395631+biswanathdas1995@users.noreply.github.com>
2022-03-11 16:40:13 +0530

    Update before_install.sh

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
-FILE=/home/ubuntu/aws-nodejs-sample-codebuild/app.js
-if test -f "$FILE"; then
-   echo "$FILE exists"
-   cd /home/ubuntu/
+   cd /home/ubuntu/aws-nodejs-sample-codebuild
    pm2 stop --silent app.js
    pm2 delete app.js
-   killall -9 node
-else
-    echo "$FILE does not exist."
-fi
-cd /home/ubuntu/
+ cd /home/ubuntu/
 sudo rm -rf aws-nodejs-sample-codebuild
 sudo mkdir aws-nodejs-sample-codebuild
